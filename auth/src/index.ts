27571ec7d0abe2d341499158257cb989a272d399
Also Ailton <ailton_gulmanelli@hotmail.com>
2023-01-05 17:02:11 -0300

    create routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,18 @@
 import express from 'express';
 import { json } from 'body-parser';
 
-const app =express ();
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/sigin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+
+const app = express ();
 app.use(json());
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.listen(3000, ()=>{
     console.log('listening on port 3000!!!');
