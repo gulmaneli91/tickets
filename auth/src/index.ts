732107cd4466de64381025b505e2c6f4319066a5
Also Ailton <ailton_gulmanelli@hotmail.com>
2023-01-06 14:47:55 -0300

    implementation of not found error file

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,6 +6,7 @@ import { signinRouter } from './routes/sigin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './midlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express ();
 app.use(json());
@@ -15,8 +16,12 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
+app.all( '*', () => {
+    throw new NotFoundError();
+});
+
 app.use(errorHandler);
 
 app.listen(3000, ()=>{
     console.log('listening on port 3000!!!');
-})
+});
